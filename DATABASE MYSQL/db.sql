e55b9a2fbf8a585984e6db6f8676e09004ffce61
juliana20 <ikomangjuliana@gmail.com>
2022-04-24 22:02:52 +0800

    #last update

--- DATABASE MYSQL/db.sql
+++ DATABASE MYSQL/db.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Community v13.1.5  (64 bit)
-MySQL - 10.4.11-MariaDB : Database - u1657744_spk_kredit_fucom_smart
+MySQL - 10.4.11-MariaDB : Database - spk_kredit_fucom_smart
 *********************************************************************
 */
 
@@ -12,9 +12,9 @@ MySQL - 10.4.11-MariaDB : Database - u1657744_spk_kredit_fucom_smart
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`u1657744_spk_kredit_fucom_smart` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`spk_kredit_fucom_smart` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
-USE `u1657744_spk_kredit_fucom_smart`;
+USE `spk_kredit_fucom_smart`;
 
 /*Table structure for table `tb_alternatif` */
 
@@ -22,13 +22,38 @@ DROP TABLE IF EXISTS `tb_alternatif`;
 
 CREATE TABLE `tb_alternatif` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_hasil` int(11) DEFAULT NULL,
-  `nama_alternatif` varchar(100) DEFAULT NULL,
-  `bobot_alternatif` int(11) DEFAULT NULL,
+  `kode_alternatif` varchar(11) DEFAULT NULL,
+  `id_pengajuan` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `tb_alternatif` */
+
+insert  into `tb_alternatif`(`id`,`kode_alternatif`,`id_pengajuan`) values 
+(1,'A1',1),
+(2,'A2',2);
+
+/*Table structure for table `tb_group_kriteria` */
+
+DROP TABLE IF EXISTS `tb_group_kriteria`;
+
+CREATE TABLE `tb_group_kriteria` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kode_kriteria` varchar(20) DEFAULT NULL,
+  `nama_kriteria` varchar(100) DEFAULT NULL,
+  `jenis_kriteria` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `tb_group_kriteria` */
+
+insert  into `tb_group_kriteria`(`id`,`kode_kriteria`,`nama_kriteria`,`jenis_kriteria`) values 
+(1,'C1','Jaminan','Keuntungan'),
+(2,'C2','Karakter','Keuntungan'),
+(3,'C3','Pendapatan','Keuntungan'),
+(4,'C4','Pengeluaran','Kerugian'),
+(5,'C5','Kemampuan','Keuntungan'),
+(6,'C6','Kondisi Hutang','Keuntungan');
 
 /*Table structure for table `tb_hasil` */
 
@@ -51,13 +76,20 @@ CREATE TABLE `tb_kriteria` (
   `nama_kriteria` varchar(100) DEFAULT NULL,
   `bobot_kriteria` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `tb_kriteria` */
 
 insert  into `tb_kriteria`(`id`,`kode_kriteria`,`nama_kriteria`,`bobot_kriteria`) values 
-(1,'C1','Karakter',20),
-(2,'C2','Jaminan',15);
+(1,'C1','Surat Pegawai',5),
+(2,'C1','Surat Bangunan',4),
+(3,'C1','BPKB Mobil',3),
+(4,'C1','BPKB Motor',2),
+(5,'C1','Tidak Ada',1),
+(6,'C2','Bertanggung Jawab',2),
+(7,'C2','Kurang Dipercaya',1),
+(8,'C6','Tidak',2),
+(9,'C6','Ada',1);
 
 /*Table structure for table `tb_nasabah` */
 
@@ -73,12 +105,13 @@ CREATE TABLE `tb_nasabah` (
   `jenis_kelamin` varchar(20) DEFAULT NULL,
   `agama` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `tb_nasabah` */
 
 insert  into `tb_nasabah`(`id`,`id_nasabah`,`nama_nasabah`,`alamat_nasabah`,`telepon`,`no_ktp`,`jenis_kelamin`,`agama`) values 
-(1,'NB00001','I Wayan Jana Antara','Denpasar Timur','081999897666','01239131231222','L','Hindu');
+(1,'NB00001','I Wayan Jana Antara','Denpasar Timur','081999897666','01239131231222','L','Hindu'),
+(2,'NB00002','I Wayan Jana','Denpasar Timur','081999897565','01239131231223','L','Hindu');
 
 /*Table structure for table `tb_pengajuan` */
 
@@ -86,18 +119,23 @@ DROP TABLE IF EXISTS `tb_pengajuan`;
 
 CREATE TABLE `tb_pengajuan` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_pengajuan` varchar(100) DEFAULT NULL,
   `id_nasabah` int(11) DEFAULT NULL,
   `tgl_pengajuan` date DEFAULT NULL,
   `jaminan` varchar(100) DEFAULT NULL,
   `karakter` varchar(100) DEFAULT NULL,
   `kemampuan` varchar(50) DEFAULT NULL,
-  `pendapatan` float DEFAULT NULL,
-  `pengeluaran` float DEFAULT NULL,
+  `pendapatan` varchar(50) DEFAULT NULL,
+  `pengeluaran` varchar(50) DEFAULT NULL,
   `kondisi_hutang` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `tb_pengajuan` */
+
+insert  into `tb_pengajuan`(`id`,`id_pengajuan`,`id_nasabah`,`tgl_pengajuan`,`jaminan`,`karakter`,`kemampuan`,`pendapatan`,`pengeluaran`,`kondisi_hutang`) values 
+(1,'PGJ000001',1,'2022-04-17','1','6','4','2','3','9'),
+(2,'PGJ000002',2,'2022-04-17','3','6','4','2','3','8');
 
 /*Table structure for table `tb_pengguna` */
 
